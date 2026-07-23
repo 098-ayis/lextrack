3b234f16207cff391875fb891d8cf98412f2aba8
098-ayis <lobarbioayis19@gmail.com>
2026-07-23 17:46:18 +0800

    db migrations

--- database/migrations/2026_06_23_153913_create_documents_table.php
+++ database/migrations/2026_06_23_153913_create_documents_table.php
@@ -12,9 +12,42 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('documents', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
+            $table->id('document_id');
+
+            $table->foreignId('user_id')
+                ->constrained()
+                ->restrictOnDelete();
+
+            $table->foreignId('type_id')
+                ->constrained('document_types')
+                ->restrictOnDelete();
+
+            $table->foreignId('client_id')
+                ->constrained()
+                ->restrictOnDelete();
+
+            $table->string('lao_number')->nullable();
+            $table->string('office_unit')->nullable();
+            $table->text('particulars');
+
+            $table->date('deadline')->nullable();
+
+            $table->string('action_taken')->nullable();
+
+            $table->string('sent_to')->nullable();
+            $table->date('sent_date')->nullable();
+
+            $table->string('returned_from')->nullable();
+            $table->date('date_returned')->nullable();
+
+            $table->date('outgoing_date')->nullable();
+
+            $table->enum('status',
+                ['pending', 'received', 'ongoing', 'completed', 'returned', 'archived']
+            )->default('pending');
+
             $table->timestamps();
+            
         });
     }
 
---
+++ database/migrations/2026_07_23_092749_create_notifications_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id('notification_id');
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->string('notification_title');
+            $table->text('body');
+            $table->timestamps('created_at');
+            $tabley->boolean('is_read')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('notifications');
+    }
+};
