0b93ab0a53c8b591b57c4253f871c91b271edcd8
Hershey <hah2023-3276-22539@bicol-u.edu.ph>
2026-08-04 17:16:41 +0800

    import the router, need nyo maginstall router vue for this

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
-import HomePage from './components/HomePage.vue'
+import App from './App.vue'
+import router from './router'
+import '../css/app.css'
 
-createApp(HomePage).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
