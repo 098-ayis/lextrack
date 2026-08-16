73b6d2b38f9a9f1ae9e5aac3864628c99ed2bdce
098-ayis <lobarbioayis19@gmail.com>
2026-08-16 23:32:10 +0800

    sign out landing page

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use App\Http\Responses\LogoutResponse;
+use Filament\Auth\Http\Responses\Contracts\LogoutResponse as LogoutResponseContract;
 use Illuminate\Support\ServiceProvider;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            LogoutResponseContract::class,
+            LogoutResponse::class,
+        );
     }
 
     /**
